9981c980c1a44fb938650699e771ca174e277b03
Hariic33 <hmakbets@gmail.com>
2023-11-21 17:27:48 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+
+const App = () => {
+  const [inputText, setInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+
+  const handleButtonPress = () => {
+    setOutputText((prevText) => `${prevText}\n${inputText}`);
+    setInputText('');
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter text"
+        value={inputText}
+        onChangeText={(text) => setInputText(text)}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
+        <Text style={styles.buttonText}>Press me</Text>
+      </TouchableOpacity>
+      <ScrollView style={styles.outputContainer}>
+        <Text style={styles.outputText}>{outputText}</Text>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    height: 40,
+    borderColor: '#bdc3c7',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 8,
+    width: '100%',
+    borderRadius: 8,
+    backgroundColor: '#ecf0f1',
+  },
+  button: {
+    backgroundColor: '#3498db',
+    padding: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  outputContainer: {
+    maxHeight: 200,
+    width: '100%',
+    backgroundColor: '#ecf0f1',
+    borderWidth: 1,
+    borderColor: '#bdc3c7',
+    borderRadius: 8,
+  },
+  outputText: {
+    fontSize: 14,
+    marginLeft: 20,
+  },
+});
+
+export default App;
